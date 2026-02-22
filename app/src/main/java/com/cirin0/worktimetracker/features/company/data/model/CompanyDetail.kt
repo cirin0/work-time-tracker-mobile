03f95d6c26f4b21041000fbcfb26bb3e9c84b245
cirin0 <ivan.gruziv@gmail.com>
2026-02-22 18:34:44 +0200

    feat: implement custom deserializer for employees in CompanyDetail model

--- app/src/main/java/com/cirin0/worktimetracker/features/company/data/model/CompanyDetail.kt
+++ app/src/main/java/com/cirin0/worktimetracker/features/company/data/model/CompanyDetail.kt
@@ -1,6 +1,11 @@
 package com.cirin0.worktimetracker.features.company.data.model
 
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
+import java.lang.reflect.Type
 
 data class CompanyDetail(
     val id: Int,
@@ -15,6 +20,7 @@ data class CompanyDetail(
     @SerializedName("radius_meters")
     val radiusMeters: Int?,
     val manager: BaseUser,
+    @JsonAdapter(EmployeesDeserializer::class)
     val employees: List<BaseUser>?,
     @SerializedName("employee_count") val usersCount: Int,
     @SerializedName("created_at") val createdAt: String,
@@ -33,3 +39,32 @@ data class BaseUser(
     val email: String,
     val avatar: String?
 )
+
+class EmployeesDeserializer : JsonDeserializer<List<BaseUser>> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): List<BaseUser>? {
+        if (json == null || json.isJsonNull) {
+            return null
+        }
+
+        return when {
+            json.isJsonArray -> {
+                json.asJsonArray.map { element ->
+                    context!!.deserialize(element, BaseUser::class.java)
+                }
+            }
+
+            json.isJsonObject -> {
+                json.asJsonObject.entrySet().map { (_, value) ->
+                    context!!.deserialize(value, BaseUser::class.java)
+                }
+            }
+
+            else -> null
+        }
+    }
+}
+
